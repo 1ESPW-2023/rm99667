b192ad38d3bc55054d64d31c79adef596eff4b5f
Blue260910 <rm99667@fiap.com.br>
2023-04-11 21:05:31 -0300

    Criação de functions

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
-function myFunction() {
-    document.write("Hello World!");
-  }
+const divElement = document.getElementById("div-box")
+
+divElement.addEventListener("click",Soma );
+
+function MostraElemento(){
+    console.log(divElement)
+}
+
+function Soma (){
+    var n1 = 10
+    var n2 = 20
+
+    console.log(n1 + n2)
+
+}
