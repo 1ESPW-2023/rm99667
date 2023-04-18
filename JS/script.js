5f6249c941d30f1395661a706e7c276b3bce0df0
Blue260910 <rm99667@fiap.com.br>
2023-04-18 20:23:40 -0300

    Utilizando Querry

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,32 @@
-const divElement = document.getElementById("div-box")
+// const divElement = document.getElementById("div-box")
 
-divElement.addEventListener("click",Soma );
+// divElement.addEventListener("click",Soma );
 
-function MostraElemento(){
-    console.log(divElement)
-}
+// function MostraElemento(){
+//     console.log(divElement)
+// }
 
-function Soma (){
-    var n1 = 10
-    var n2 = 20
+// function Soma (){
+//     var n1 = 10
+//     var n2 = 20
 
-    console.log(n1 + n2)
+//     console.log(n1 + n2)
 
-}
+// }
+
+// // Recuperar um elemento com querySelector
+// const LiElements = document.querySelector("li); 
+
+// Convertendo Em um Array e simplificando 
+// const LiElements = document.querySelectorAll("li"); 
+// const ArrayElements = [...LiElements];
+
+const ArrayElements = [...document.querySelectorAll("li")];
+
+ArrayElements.map((El, key)=>{
+    if (El.textContent == 'Item-25')(
+        El.textContent = "O item 25 é esse"
+    )
+
+
+});
